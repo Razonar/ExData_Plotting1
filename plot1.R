dace73a159796c2912400a2e226acb3a2e4be168
Razonar <razonar@outlook.com>
2020-08-06 15:14:01 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,7 @@
+download <- download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "temp")
+unzip("temp")
+unlink("temp")
+
 b <- read.csv(file="household_power_consumption.txt",sep=";",colClasses="character")
 v <- b[b$Date == "1/2/2007"| b$Date =="2/2/2007",]
 v$Date <- as.Date(v$Date,"%d/%m/%Y")
