87c4ddfc34021bb9c7509f475914bc878bfbbb3b
Razonar <razonar@outlook.com>
2020-08-06 15:16:28 -0300

    Files for Course Project 1

--- plot1.R
+++ plot1.R
@@ -9,3 +9,4 @@ v$Global_active_power <- as.numeric(v$Global_active_power)
 png("plot1.png")
 hist(v$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
 dev.off()
+
